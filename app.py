b88133e9f8a8e3337e91c01af31e74725e2949ac
Noor8Student <noor8@purdue.edu>
2026-05-01 14:34:20 -0400

    fjdsklfj

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ if __name__ == "__main__":
     if not os.path.exists(DATABASE):
         init_db()
     if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=10000)
+        app.run(host="0.0.0.0", port=8080)
